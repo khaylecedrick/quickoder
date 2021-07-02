bafc2a55a75236bb141e429ce25f1395cbd6d97e
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-07-02 15:14:08 +0800

    Team 2 components initial commit

---
+++ COMPONENTS (BRUCE)/PAGINATIONS/BRUCE-pagination-1.blade.php.php
@@ -0,0 +1,29 @@
+<x-guest-layout>
+    <div class="flex items-center justify-center w-screen h-screen px-32 bg-white dark:bg-black">
+        <div class="w-screen h-20 px-4  bg-gray-100 border rounded-lg">         
+            <div class="flex flex-row items-center justify-between py-2 mt-6 border-t-2 border-blue-900 opacity-75">
+            <div class="flex justify-items-start">
+            <button class="flex pl-6 font-sans text-gray-500 opacity-75 flex-rows">
+                <svg class="w-6 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+            Previous</button>
+            </div>
+        
+            <div class="flex flex-row space-x-10 justify-items-center">
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">1</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">2</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">3</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">...</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">8</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">9</a>
+                <a href=""class="px-2 text-xl text-gray-500 transform rounded hover:text-gray-800 hover:bg-gray-300 hover:font-bold">10</a>
+            </div>
+            
+            <div class="flex justify-items-end">
+            <button class="flex pr-6 font-sans text-gray-500 opacity-75 flex-rows">Next
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>           
+            </button>
+            </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
---
+++ COMPONENTS (BRUCE)/PAGINATIONS/BRUCE-pagination-2.blade.php.php
@@ -0,0 +1,41 @@
+<x-guest-layout>
+    <div class="flex items-center justify-center w-screen h-screen px-32 bg-white">
+        <div class="w-screen h-20 px-4 bg-gray-100 border rounded-lg dark:bg-black"> 
+            <div class="flex justify-between w-auto h-16 mt-2 bg-purple-200 border rounded justify-items-center"> 
+                <div class="flex flex-row items-center ">
+                    <div class="flex justify-items-start">
+                        <button class="flex pl-6 font-serif text-xs text-blue-900 flex-rows">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="rotate-180 w-9 h-9" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+                              </svg>
+                              <div class="flex flex-row mt-1 text-xl justify-items-start">Previous</div>
+                        </button>
+                    </div>
+                </div>
+                <div class="flex flex-row items-center ">
+                    <div class="flex flex-row space-x-5 sm:space-x-3 justify-items-center">
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">1</a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">2</a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">3</a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">... </a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">8</a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">9</a>
+                            <a href=""class="w-12 h-12 font-mono text-4xl text-blue-900 transform rounded-full hover:text-indigo-900 hover:bg-white hover:flex hover:justify-center hover:items-center">10</a>
+                        </div>
+                </div>
+                            
+                <div class="flex flex-row items-center ">
+                        <div class="flex justify-items-end">
+                        <button class="flex pr-6 font-serif text-xs text-blue-900 flex-rows">
+                            <div class="flex flex-row mt-1 text-xl justify-items-start">Next</div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class=" w-9 h-9" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                        </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
